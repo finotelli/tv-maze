6f125f4737e10e47ab9cc0b1017334cd50dab86f
Ana Finotelli <finotelli2004@gmail.com>
2023-05-31 00:56:53 -0300

    tv-maze

--- index.js
+++ index.js
@@ -1,22 +1,50 @@
 const handleSearch = async (event) => {
   event.preventDefault();
 
-  // implemente a consulta a partir daqui
-
-  //// Exemplo de endpoint: https://api.tvmaze.com/search/shows?q=lost
-
-  //// Elementos de leiaute importantes:
-
-  //  #message: use para exibir mensagens aos usuário, por exemplo:
-
+  //EXIBIR MENSAGEM DE CARREGAMENTO//
   const message = document.querySelector('#message');
-  message.innerHTML = 'exercício ainda não resolvido.';
+  message.innerHTML = 'buscando...';
 
-  //  #shows: conterá os shows, cada um em um <li>, por exemplo:
-  // <li>
-  //   <img class="poster" src="https://static.tvmaze.com/uploads/images/medium_portrait/0/1389.jpg" />
-  //   <span class="show-name">Lost</span>
-  // </li>
+  //LIMPA LISTA DE PROGRAMAS//
+  const listaDeSeries = document.querySelector('#shows');
+  listaDeSeries.innerHTML = '';
+
+  const Busca = document.querySelector('#query');
+  const textoASerBuscado = Busca.value;
+
+  //URL DE CONSULTA//
+  const url = `https://api.tvmaze.com/search/shows?q=${textoASerBuscado}`;
+
+  //REALIZAR A CONSULTA NA API//
+  const resposta = await fetch(url);
+  const series = await resposta.json();
+
+  //FINALIZA O PROCEDIMENTO SE NAO TIVER NENHUM RESULTADO//
+  if (series.length == 0) {
+    //EXIBIR MENSAGEM DE NAO ENCONTRADO//
+    message.innerHTML = 'nenhum resultado encontrado.';
+    return;
+  }
+
+  //LIMPA//
+  message.innerHTML = '';
+  //ITERAR PELO PROGRAMA//
+  series.forEach((series) => {
+    //OBTER OS DADOS//
+    const titulo = series?.show?.name || '';
+    const imagem = series?.show?.image.medium || '';
+
+    //INSERIR OS PROGRAMAS NA LISTA DE RESULTADO//
+    listaDeSeries.insertAdjacentHTML(
+      'beforeend',
+      `
+  <li>
+    <img class="poster" src="${imagem}">
+    <span class="show-name">${titulo}</span>
+  </li>
+  `
+    );
+  });
 };
 
 document.addEventListener('DOMContentLoaded', () => {
